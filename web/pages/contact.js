90c7eb5a5b89ab213aa1a7de1eed9088f86c4376
Michael Bonner <michaelwbonner@gmail.com>
2021-09-27 17:51:32 -0600

    simple contact page

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -5,43 +5,39 @@ import BlockContent from '@sanity/block-content-to-react'
 import Layout from '../components/layout'
 import { getClient } from '../lib/sanity'
 import urlForSanitySource from '../lib/urlForSanitySource'
+import { H1 } from '../components/headings'
 
-function Home({ about }) {
+function Home({ contact }) {
+    const backgroundImageUrl = urlForSanitySource(contact.backgroundImage)
+        .width(1400)
+        .url()
     return (
-        <Layout title={about.seoTitle} description={about.seoDescription}>
-            <div className="max-w-5xl mx-auto px-4 lg:px-0">
-                <div className="relative border-2 border-black py-8 px-7 max-w-xs mx-auto w-full">
-                    <img
-                        src={urlForSanitySource(about.photo).width(500).url()}
-                        alt="Jeremy Miller Headshot"
-                    />
-                    <div className="absolute px-4 w-full left-0 right-0 -bottom-6">
-                        <div className="flex items-center justify-center w-full">
-                            <div className="bg-white px-8">
-                                <Image
-                                    alt="x"
-                                    className="bg-white px-8"
-                                    height="30"
-                                    objectFit="cover"
-                                    src="/images/menu-close.svg"
-                                    width="30"
-                                />
-                            </div>
-                        </div>
+        <Layout title={contact.seoTitle} description={contact.seoDescription}>
+            <div
+                style={{
+                    backgroundColor: '#ccc',
+                    backgroundImage: `url(${backgroundImageUrl})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                }}
+            >
+                <div className="max-w-5xl mx-auto px-4 lg:px-0 py-24 text-center">
+                    <H1>{contact.title}</H1>
+                </div>
+            </div>
+            <div>
+                <div className="max-w-5xl mx-auto px-4 lg:px-0 py-24 text-center">
+                    <div>
+                        <p>JME STUDIO ADDRESS</p>
+                        <p>1589 W 2225 S</p>
+                        <p>WOODS CROSS, UT 84087</p>
+                    </div>
+                    <div>
+                        <p>STUDIO LINE: 801-797-9023</p>
+                        <p>DIRECT LINE: 801-971-4683</p>
+                        <p>EMAIL: INFO@JMILLSENT.COM</p>
                     </div>
                 </div>
-                <BlockContent
-                    className="my-16 prose max-w-3xl text-center mx-auto"
-                    blocks={about.bio}
-                />
-                <BlockContent
-                    className="my-16 prose max-w-3xl text-center mx-auto"
-                    blocks={about.representation}
-                />
-                <BlockContent
-                    className="my-16 prose max-w-5xl text-center mx-auto"
-                    blocks={about.notableAwards}
-                />
             </div>
         </Layout>
     )
@@ -50,13 +46,10 @@ function Home({ about }) {
 export async function getStaticProps() {
     return {
         props: {
-            about: await getClient().fetch(groq`
-          *[_type == "aboutPage"][0]{
+            contact: await getClient().fetch(groq`
+          *[_type == "contactPage"][0]{
             title,
-            photo,
-            bio,
-            representation,
-            notableAwards,
+            backgroundImage,
             seoTitle,
             seoDescription
           }

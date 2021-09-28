fff782550581f99796841e868d1d90997c8b6a7b
Michael Bonner <michaelwbonner@gmail.com>
2021-09-27 19:53:53 -0600

    contact page

---
+++ web/components/map.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import mapStyles from '../data/map-styles'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+
+const Map = ({ marker }) => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || '',
+    })
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={{
+                width: '100%',
+                height: '400px',
+                maxHeight: `800px`,
+            }}
+            center={{ lat: 40.8309715, lng: -111.9024321 }}
+            zoom={11}
+            options={{
+                styles: mapStyles,
+            }}
+        >
+            <Marker
+                icon={marker.icon}
+                position={{ lat: marker.lat, lng: marker.lng }}
+            />
+        </GoogleMap>
+    ) : (
+        <></>
+    )
+}
+
+export default Map
---
+++ web/lib/send-ses-email.js
@@ -0,0 +1,42 @@
+var AWS = require('aws-sdk')
+// Set the region
+AWS.config.update({
+    accessKeyId: process.env.AWS_CONFIG_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_CONFIG_SECRET_KEY,
+    region: process.env.AWS_CONFIG_REGION,
+    domain: 'ravens.works',
+})
+
+const sendSesEmail = async (to, from, subject, contentHtml, contentTxt) => {
+    var params = {
+        Destination: {
+            /* required */
+            ToAddresses: to,
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: contentHtml,
+                },
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: contentTxt,
+                },
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: subject,
+            },
+        },
+        Source: from,
+        ReplyToAddresses: [from],
+    }
+
+    // Create the promise and SES service object
+    return await new AWS.SES({ apiVersion: '2010-12-01' })
+        .sendEmail(params)
+        .promise()
+}
+
+export default sendSesEmail

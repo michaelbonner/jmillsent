c28acbc5caeff0611344e5dd43c9319d4bd6cba3
Michael Bonner <michaelwbonner@gmail.com>
2022-04-04 20:50:59 -0600

    fix duplicate class names

--- web/pages/work/[slug].js
+++ web/pages/work/[slug].js
@@ -102,7 +102,7 @@ const WorkItem = ({ workItem = {} }) => {
                         key={index}
                       >
                         <div className="font-bold uppercase">{credit.role}</div>
-                        <div className="uppercase space-x-4 space-x-4 font-outline tracking-wide">
+                        <div className="uppercase space-x-4 font-outline tracking-wide">
                           {credit.value}
                         </div>
                       </div>
@@ -117,7 +117,7 @@ const WorkItem = ({ workItem = {} }) => {
                         key={index}
                       >
                         <div className="font-bold uppercase">{credit.role}</div>
-                        <div className="uppercase space-x-4 space-x-4 font-outline tracking-wide">
+                        <div className="uppercase space-x-4 font-outline tracking-wide">
                           {credit.value}
                         </div>
                       </div>

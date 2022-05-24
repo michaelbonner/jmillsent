633650d4acb66178d48cf5c2d6d16fb039c39f37
Colton Bonner <cbonner26118@gmail.com>
2022-05-23 19:03:06 -0700

    Added boolean toggle for alignment of text on studio items.

--- studio/schemas/studioItem.js
+++ studio/schemas/studioItem.js
@@ -1,40 +1,47 @@
-import { MdWeb as icon } from "react-icons/md";
+import { MdWeb as icon } from 'react-icons/md';
 
 export default {
-  name: "studioItem",
-  title: "Studio Spaces",
-  type: "document",
+  name: 'studioItem',
+  title: 'Studio Spaces',
+  type: 'document',
   fields: [
     {
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Title',
+      type: 'string',
     },
     {
-      name: "description",
-      title: "Description",
-      type: "array",
-      of: [{ type: "block" }],
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'block' }],
     },
     {
-      name: "image",
-      title: "Image",
-      type: "image",
+      name: 'image',
+      title: 'Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
     },
+    {
+      name: 'rightAlign',
+      title: 'Right Align',
+      type: 'boolean',
+      description: 'If checked, the text will be aligned to the right',
+      initialValue: false,
+    },
   ],
   preview: {
     select: {
-      clientName: "clientName",
-      title: "title",
-      media: "poster",
+      clientName: 'clientName',
+      title: 'title',
+      media: 'poster',
     },
     prepare(selection) {
       return {
-        title: `${selection.clientName ? `${selection.clientName} | ` : ""}${
-          selection.title || ""
+        title: `${selection.clientName ? `${selection.clientName} | ` : ''}${
+          selection.title || ''
         }`,
         date: selection.date,
         subtitle: selection.date,

6017f4a4d924ce2de95d2bb5a4cbc43286433f98
Michael Bonner <michaelwbonner@gmail.com>
2022-05-25 14:05:07 -0600

    format studio/schemas/service.js

--- studio/schemas/service.js
+++ studio/schemas/service.js
@@ -1,49 +1,49 @@
-import { MdDeveloperBoard as icon } from 'react-icons/md';
+import { MdDeveloperBoard as icon } from "react-icons/md";
 
 export default {
-  name: 'service',
-  title: 'Services',
-  type: 'document',
+  name: "service",
+  title: "Services",
+  type: "document",
   icon,
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-      description: 'Name of service',
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "Name of service",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'name',
+        source: "name",
         maxLength: 100,
       },
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'array',
-      of: [{ type: 'block' }],
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
     },
     {
-      name: 'rightAlign',
-      title: 'Right Align',
-      type: 'boolean',
-      description: 'If checked, the text will be aligned to the right',
+      name: "rightAlign",
+      title: "Right Align",
+      type: "boolean",
+      description: "If checked, the text will be aligned to the right",
       initialValue: false,
     },
   ],
   preview: {
-    select: { title: 'name', media: 'image' },
+    select: { title: "name", media: "image" },
   },
 };

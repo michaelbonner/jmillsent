9ad6a57862ce147c9d35a15041907df5265ce796
Michael Bonner <michaelwbonner@gmail.com>
2022-06-08 13:21:47 -0600

    footer nav vertical spacing

--- web/components/layout.js
+++ web/components/layout.js
@@ -437,8 +437,8 @@ const Layout = ({
       <main className="bg-black text-white relative z-10 w-full">
         {children}
       </main>
-      <footer className="bg-black relative z-10 pb-8 text-center">
-        <nav className="w-full container max-w-5xl px-12 lg:px-4 pb-8 lg:mx-auto flex flex-wrap justify-center lg:justify-around space-x-4">
+      <footer className="bg-black relative z-10 text-center">
+        <nav className="w-full container max-w-5xl px-12 lg:px-4 pb-8 lg:mx-auto flex flex-wrap justify-center lg:justify-around gap-4">
           {navItems.map((navItem, index) => {
             return (
               <Link key={index} href={navItem.href}>

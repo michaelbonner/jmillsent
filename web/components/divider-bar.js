b8b3b79c2b7aaedafb16aee4222b0ff4b9e08040
Michael Bonner <michaelwbonner@gmail.com>
2022-06-10 11:35:51 -0600

    gold divider line with gold x

--- web/components/divider-bar.js
+++ web/components/divider-bar.js
@@ -14,7 +14,7 @@ const DividerBar = ({ yMargin = 'my-24 lg:my-36' }) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          className="text-gray-300 fill-current"
+          className="text-gold fill-current"
           d="m153.2 205.4-43.5-67.2-43.5 67.2h-61.2l69.9-102.6-65.4-97.5h60.9l39.3 62.7 38.7-62.7h61.2l-65.1 97.2 69.6 102.9z"
         />
       </svg>

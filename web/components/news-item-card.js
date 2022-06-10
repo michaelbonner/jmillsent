a9cb29772cae33e132bad7287a2f40ce2a08214b
Colton Bonner <cbonner26118@gmail.com>
2022-06-10 16:39:24 -0700

    Refactored news item and page to ensure no funky aspect ratio changes. Adjusted spacing between each card Removed unnecessary CSS for news cards.

--- web/components/news-item-card.js
+++ web/components/news-item-card.js
@@ -1,32 +1,24 @@
-import classNames from 'classnames'
 import Link from 'next/link'
 import urlForSanitySource from '../lib/urlForSanitySource'
 import Date from './date'
 import LargeGoldBar from './large-gold-bar'
+import Image from 'next/image'
 
-const NewsItemCard = ({ newsItem, index, hideAfterCount = 999 }) => {
+const NewsItemCard = ({ newsItem }) => {
   return (
     <>
-      <div className="z-10 w-full mt-4 lg:mt-10 lg:max-w-3xl xl:max-w-5xl">
+      <div className="z-10 w-full lg:max-w-2xl xl:max-w-4xl">
         <Link href={`/news/${newsItem.slug.current}`} key={newsItem._id}>
-          <a
-            className={classNames(
-              {
-                'lg:hidden': index >= hideAfterCount,
-              },
-              `bpd-news-tile`,
-              `flex flex-col items-center justify-center`
-            )}
-            key={newsItem._id}
-            style={{
-              backgroundImage: newsItem.poster
-                ? `url(${urlForSanitySource(newsItem.poster).width(700)})`
-                : '',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-            }}
-          ></a>
+          <a key={newsItem._id}>
+            <Image
+              alt={newsItem.seoTitle}
+              src={`${urlForSanitySource(
+                newsItem.heroImage || newsItem.poster
+              )}?w=1440&h=600&auto=format&fit=crop&crop=focalpoint`}
+              height={600}
+              width={1440}
+            />
+          </a>
         </Link>
         <div className="mx-auto lg:max-w-2xl xl:max-w-4xl">
           <div className="flex flex-nowrap justify-center items-center px-10 text-lg gap-x-4 sm:text-3xl uppercase z-10 mt-10">

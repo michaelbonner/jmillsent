1393e1f26d1ee28c36421a26948b77d636c300f3
Colton Bonner <cbonner26118@gmail.com>
2022-06-20 15:00:39 -0600

    Redesigned services section to have text underneath images. Created badge svg and positioned badge & incrementing service step absolute to card.

--- web/components/background-text-section-half.js
+++ web/components/background-text-section-half.js
@@ -10,26 +10,22 @@ const BackgroundTextSectionHalf = ({
   imageAlt,
   title,
   description,
-  leftOrRight = 'left',
+  step,
 }) => {
-  const styles = {
-    left: {
-      textAlign: 'text-center lg:text-left',
-      barPosition: 'mx-auto lg:ml-0 lg:mr-auto',
-      containerPosition: 'left-0 lg:pl-4',
-      gradientDirection: 'bg-gradient-to-r',
-    },
-    right: {
-      textAlign: 'text-center lg:text-right',
-      barPosition: 'mx-auto lg:mr-0 lg:ml-auto',
-      containerPosition: 'right-0 lg:pr-4',
-      gradientDirection: 'bg-gradient-to-l',
-    },
-  }
-  const titleStyle = leftOrRight === 'left' ? 'lg:text-left' : 'lg:text-right'
   return (
-    <div className="border p-4 lg:p-6 border-white">
-      <div className="w-full relative group" style={{ lineHeight: 0 }}>
+    <div className="border relative p-4 lg:p-6 border-white">
+      <div className="absolute z-10 left-0 top-0 pl-10 text-center">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/images/simple-badge-gold.svg"
+            alt="Service Step"
+            width={50}
+            height={75}
+          />
+          <p className="absolute text-black text-2xl font-extrabold">{step}</p>
+        </div>
+      </div>
+      <div className="w-full group" style={{ lineHeight: 0 }}>
         <Image
           alt={imageAlt}
           height={800}
@@ -38,20 +34,13 @@ const BackgroundTextSectionHalf = ({
           )}?w=1246&h=800&auto=format&fit=crop&crop=focalpoint`}
           width={1246}
         />
-        <div
-          className={`lg:absolute w-full ${styles[leftOrRight].containerPosition} top-0 bottom-0 h-full ${styles[leftOrRight].gradientDirection} from-black to-transparent opacity-70 group-hover:opacity-80 transition-all duration-500`}
-        />
-        <div
-          className={`lg:absolute ${styles[leftOrRight].containerPosition} top-0 bottom-0 ${styles[leftOrRight].textAlign} flex flex-col justify-center items-start sm:gap-y-2 lg:max-w-[70%]`}
-        >
-          <H3
-            className={`${titleStyle} inline !mt-3 w-full uppercase text-center !mb-0 py-1`}
-          >
+        <div className="flex flex-col justify-center mx-auto gap-y-2 lg:max-w-[80%]">
+          <H3 className="inline !mt-3 w-full uppercase text-center !mb-0 py-1">
             {title}
           </H3>
           {description && (
             <>
-              <div className={`${styles[leftOrRight].barPosition}`}>
+              <div className="mx-auto">
                 <LittleGoldBar />
               </div>
               <div className="max-w-lg prose-lg mx-auto text-white font-light leading-normal mt-1.5">

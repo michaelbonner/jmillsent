d1864e95616f2060318aa16df3b45502214b2720
Michael Bonner <michaelwbonner@gmail.com>
2022-07-01 16:36:20 -0600

    servcies section on desktop

--- web/components/background-text-section-half.js
+++ web/components/background-text-section-half.js
@@ -1,5 +1,6 @@
 import urlForSanitySource from '@/lib/urlForSanitySource'
 import { PortableText } from '@portabletext/react'
+import useIsDesktop from 'hooks/useIsDesktop'
 import Image from 'next/image'
 import React from 'react'
 import { GrCheckbox, GrCheckboxSelected } from 'react-icons/gr'
@@ -14,18 +15,25 @@ const BackgroundTextSectionHalf = ({
   step,
   serviceShortNames,
 }) => {
+  const isDesktop = useIsDesktop()
   return (
     <div className="border relative p-4 lg:p-6 border-gray-300">
       <div className="w-full group" style={{ lineHeight: 0 }}>
         <div className="relative">
           <div className="absolute inset-0 z-10 bg-gradient-to-l from-black via-transparent to-transparent bg-opacity-40" />
-          <div className="absolute top-0 right-4 bottom-0 left-auto flex flex-col justify-center gap-y-2 z-10 text-right text-xs">
+          <div className="absolute top-0 right-4 bottom-0 left-auto flex flex-col justify-center gap-y-2 z-10 text-right text-xs lg:text-sm">
+            <div className="hidden lg:block">
+              <h3 className="text-3xl font-bold">{title}</h3>
+              <div className="flex justify-end py-1">
+                <LittleGoldBar />
+              </div>
+            </div>
             {serviceShortNames?.map((serviceShortName, index) => {
               const isChecked = index + 1 < step
               const isCurrent = index + 1 === step
               return (
                 <div
-                  className={`flex items-center gap-x-2 justify-end ${
+                  className={`flex items-center gap-x-2 lg:gap-x-4 justify-end ${
                     isCurrent ? 'text-white font-bold' : 'text-gray-200'
                   }`}
                   key={index}
@@ -39,7 +47,7 @@ const BackgroundTextSectionHalf = ({
                   </p>
                   {isChecked ? (
                     <svg
-                      className="w-4 h-4"
+                      className="w-4 h-4 lg:w-6 lg:h-6"
                       enableBackground="new 0 0 120 120"
                       viewBox="0 0 120 120"
                       xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +65,7 @@ const BackgroundTextSectionHalf = ({
                     </svg>
                   ) : (
                     <svg
-                      className="w-4 h-4"
+                      className="w-4 h-4 lg:w-6 lg:h-6"
                       enableBackground="new 0 0 120 120"
                       viewBox="0 0 120 120"
                       xmlns="http://www.w3.org/2000/svg"
@@ -73,38 +81,38 @@ const BackgroundTextSectionHalf = ({
               )
             })}
           </div>
-          <div className="absolute z-10 left-0 -top-4 pl-4 text-center">
+          <div className="absolute z-10 left-0 -top-4 lg:-top-6 pl-4 text-center">
             <div className="flex flex-col items-center justify-center">
               <Image
                 src="/images/simple-badge-gold.svg"
                 alt="Service Step"
-                width={Math.floor(150 * 0.45)}
-                height={Math.floor(135 * 0.45)}
+                width={Math.floor(150 * (isDesktop ? 0.6 : 0.45))}
+                height={Math.floor(135 * (isDesktop ? 0.6 : 0.45))}
               />
-              <p className="absolute top-0 pt-2.5 text-black text-3xl font-extrabold">
+              <p className="absolute top-0 pt-2.5 lg:pt-4 text-black text-3xl lg:text-4xl font-extrabold">
                 {step}
               </p>
             </div>
           </div>
           <Image
             alt={imageAlt}
-            height={800}
-            src={`${urlForSanitySource(
-              image
-            )}?w=1246&h=800&auto=format&fit=crop&crop=focalpoint`}
-            width={1246}
+            height={isDesktop ? 500 : 800}
+            src={`${urlForSanitySource(image)}?w=1200&h=${
+              isDesktop ? 500 : 800
+            }&auto=format&fit=crop&crop=focalpoint`}
+            width={1200}
           />
         </div>
         <div className="flex flex-col justify-center mx-auto gap-y-2">
-          <H3 className="inline !mt-3 w-full uppercase text-center !mb-0 py-1">
+          <H3 className="inline lg:hidden !mt-3 w-full uppercase text-center !mb-0 py-1">
             {title}
           </H3>
           {description && (
             <>
-              <div className="mx-auto">
+              <div className="mx-auto lg:hidden">
                 <LittleGoldBar />
               </div>
-              <div className="max-w-lg prose-lg mx-auto text-white font-light leading-normal mt-1.5">
+              <div className="lg:pt-3 max-w-5xl prose-lg mx-auto text-white font-light leading-normal mt-1.5">
                 <PortableText value={description} />
               </div>
             </>

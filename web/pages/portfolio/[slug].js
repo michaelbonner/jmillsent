8e5bfca2a7c9990472151d24be0a5cbc1584151a
Colton Bonner <cbonner26118@gmail.com>
2022-11-17 16:14:07 -0700

    Moved logged in logic to custom hook. Added check on individual portfolio items slug for login.

--- web/pages/portfolio/[slug].js
+++ web/pages/portfolio/[slug].js
@@ -3,10 +3,13 @@ import { H3 } from '@/components/headings'
 import Layout from '@/components/layout'
 import MediumWhiteBar from '@/components/medium-white-bar'
 import { getClient } from '@/lib/sanity'
+import classNames from 'classnames'
 import groq from 'groq'
 import useIsDesktop from 'hooks/useIsDesktop'
+import useIsLoggedIn from 'hooks/useIsLoggedIn'
 import dynamic from 'next/dynamic'
 import Image from 'next/future/image'
+import Link from 'next/link'
 import { useState } from 'react'
 
 const VideoPlayer = dynamic(() => import('@/components/video-player'), {})
@@ -31,8 +34,9 @@ aspect-w-15	aspect-h-15
 aspect-w-16	aspect-h-16
 */
 
-const PortfolioItem = ({ portfolioItem = {} }) => {
+const PortfolioItem = ({ portfolioItem = {}, portfolioPagePassword }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [isLoggedIn] = useIsLoggedIn(portfolioPagePassword.password)
   const isDesktop = useIsDesktop()
 
   const fullTitle = portfolioItem.clientName
@@ -60,150 +64,177 @@ const PortfolioItem = ({ portfolioItem = {} }) => {
         `${fullTitle} | JmillsENT | Motion Picture Studio + Film Agency`
       }
     >
-      <div className="px-4 lg:px-8">
-        <div className="my-12 max-w-9xl border border-gray-300 p-4 px-4 xl:mx-auto xl:p-8">
-          <VideoPlayer
-            client={portfolioItem.clientName}
-            description={portfolioItem.description}
-            poster={portfolioItem.poster}
-            title={portfolioItem.title}
-            videoId={portfolioItem.videoId}
-            clientName={portfolioItem.clientName}
-            videoHeightAspectRatio={portfolioItem.videoHeightAspectRatio || '9'}
-            videoWidthAspectRatio={portfolioItem.videoWidthAspectRatio || '16'}
-          />
+      {!isLoggedIn && (
+        <div className="mx-auto min-h-screen max-w-3xl text-center">
+          <p className="mb-6 border py-12 px-8 text-lg font-semibold uppercase text-white">
+            {portfolioPagePassword.passwordInputPrompt}
+          </p>
+          <Link href="/portfolio">
+            <a
+              className={classNames(
+                'rounded-md border-2 border-gray-300 px-4 py-2 transition-colors',
+                'hover:bg-gold hover:text-black'
+              )}
+            >
+              LOG IN
+            </a>
+          </Link>
         </div>
-      </div>
+      )}
+      {isLoggedIn && (
+        <>
+          <div className="px-4 lg:px-8">
+            <div className="my-12 max-w-9xl border border-gray-300 p-4 px-4 xl:mx-auto xl:p-8">
+              <VideoPlayer
+                client={portfolioItem.clientName}
+                description={portfolioItem.description}
+                poster={portfolioItem.poster}
+                title={portfolioItem.title}
+                videoId={portfolioItem.videoId}
+                clientName={portfolioItem.clientName}
+                videoHeightAspectRatio={
+                  portfolioItem.videoHeightAspectRatio || '9'
+                }
+                videoWidthAspectRatio={
+                  portfolioItem.videoWidthAspectRatio || '16'
+                }
+              />
+            </div>
+          </div>
 
-      <div className="3xl:px-0 container mx-auto mt-4 px-4">
-        {portfolioItem.credits && portfolioItem.credits.length > 0 && (
-          <div className="my-12 mx-auto max-w-9xl">
-            {isDesktop === false && (
-              <>
-                <button
-                  onClick={(e) => {
-                    setIsOpen(!isOpen)
-                  }}
-                  className="flex place-items-center"
-                >
-                  <h3 className="text-xl font-extrabold uppercase">Credits</h3>
-                  <div className={`${isOpen ? 'rotate-90 pl-0 pr-1' : ''}`}>
-                    <p className="place-self-center pl-2 font-outline text-2xl lg:text-4xl">
-                      &gt;
-                    </p>
-                  </div>
-                </button>
-                <div className={`${isOpen ? 'visible' : 'hidden'}`}>
-                  <div className="h-auto overflow-hidden transition-all">
-                    <div className="grid grid-cols-1">
-                      <div>
-                        {column1Credits.map((credit, index) => {
-                          return (
-                            <div
-                              className="grid grid-cols-2 items-center gap-x-4 pt-4"
-                              key={index}
-                            >
-                              <div className="justify-self-end text-right font-bold uppercase">
-                                {credit.role}
-                              </div>
-                              <div className="space-x-4 text-lg font-light uppercase lg:text-xl">
-                                {credit.value}
-                              </div>
-                            </div>
-                          )
-                        })}
+          <div className="3xl:px-0 container mx-auto mt-4 px-4">
+            {portfolioItem.credits && portfolioItem.credits.length > 0 && (
+              <div className="my-12 mx-auto max-w-9xl">
+                {isDesktop === false && (
+                  <>
+                    <button
+                      onClick={(e) => {
+                        setIsOpen(!isOpen)
+                      }}
+                      className="flex place-items-center"
+                    >
+                      <h3 className="text-xl font-extrabold uppercase">
+                        Credits
+                      </h3>
+                      <div className={`${isOpen ? 'rotate-90 pl-0 pr-1' : ''}`}>
+                        <p className="place-self-center pl-2 font-outline text-2xl lg:text-4xl">
+                          &gt;
+                        </p>
                       </div>
-                      <div>
-                        {column2Credits.map((credit, index) => {
-                          return (
-                            <div
-                              className="grid grid-cols-2 items-center gap-x-4 pt-4"
-                              key={index}
-                            >
-                              <div className="justify-self-end text-right font-bold uppercase">
-                                {credit.role}
-                              </div>
-                              <div className="space-x-4 text-lg font-light uppercase lg:text-xl">
-                                {credit.value}
-                              </div>
-                            </div>
-                          )
-                        })}
+                    </button>
+                    <div className={`${isOpen ? 'visible' : 'hidden'}`}>
+                      <div className="h-auto overflow-hidden transition-all">
+                        <div className="grid grid-cols-1">
+                          <div>
+                            {column1Credits.map((credit, index) => {
+                              return (
+                                <div
+                                  className="grid grid-cols-2 items-center gap-x-4 pt-4"
+                                  key={index}
+                                >
+                                  <div className="justify-self-end text-right font-bold uppercase">
+                                    {credit.role}
+                                  </div>
+                                  <div className="space-x-4 text-lg font-light uppercase lg:text-xl">
+                                    {credit.value}
+                                  </div>
+                                </div>
+                              )
+                            })}
+                          </div>
+                          <div>
+                            {column2Credits.map((credit, index) => {
+                              return (
+                                <div
+                                  className="grid grid-cols-2 items-center gap-x-4 pt-4"
+                                  key={index}
+                                >
+                                  <div className="justify-self-end text-right font-bold uppercase">
+                                    {credit.role}
+                                  </div>
+                                  <div className="space-x-4 text-lg font-light uppercase lg:text-xl">
+                                    {credit.value}
+                                  </div>
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </>
-            )}
+                  </>
+                )}
 
-            {isDesktop && (
-              <>
-                <H3>Credits</H3>
-                <div className="h-auto overflow-hidden transition-all lg:text-xl xl:mt-12">
-                  <div className="mb-12 grid grid-cols-1 gap-x-20 gap-y-4 md:grid-cols-2 lg:grid-cols-2">
-                    <div>
-                      {column1Credits.map((credit, index) => {
-                        return (
-                          <div
-                            className="grid grid-cols-2 items-center gap-2 pt-4"
-                            key={index}
-                          >
-                            <div className="font-bold uppercase">
-                              {credit.role}
-                            </div>
-                            <div className="space-x-4 text-xl font-light uppercase">
-                              {credit.value}
-                            </div>
-                          </div>
-                        )
-                      })}
+                {isDesktop && (
+                  <>
+                    <H3>Credits</H3>
+                    <div className="h-auto overflow-hidden transition-all lg:text-xl xl:mt-12">
+                      <div className="mb-12 grid grid-cols-1 gap-x-20 gap-y-4 md:grid-cols-2 lg:grid-cols-2">
+                        <div>
+                          {column1Credits.map((credit, index) => {
+                            return (
+                              <div
+                                className="grid grid-cols-2 items-center gap-2 pt-4"
+                                key={index}
+                              >
+                                <div className="font-bold uppercase">
+                                  {credit.role}
+                                </div>
+                                <div className="space-x-4 text-xl font-light uppercase">
+                                  {credit.value}
+                                </div>
+                              </div>
+                            )
+                          })}
+                        </div>
+                        <div>
+                          {column2Credits.map((credit, index) => {
+                            return (
+                              <div
+                                className="grid grid-cols-2 items-center gap-2 pt-4"
+                                key={index}
+                              >
+                                <div className="font-bold uppercase">
+                                  {credit.role}
+                                </div>
+                                <div className="space-x-4 text-xl font-light uppercase">
+                                  {credit.value}
+                                </div>
+                              </div>
+                            )
+                          })}
+                        </div>
+                      </div>
                     </div>
-                    <div>
-                      {column2Credits.map((credit, index) => {
-                        return (
-                          <div
-                            className="grid grid-cols-2 items-center gap-2 pt-4"
-                            key={index}
-                          >
-                            <div className="font-bold uppercase">
-                              {credit.role}
-                            </div>
-                            <div className="space-x-4 text-xl font-light uppercase">
-                              {credit.value}
-                            </div>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </div>
-                </div>
-              </>
+                  </>
+                )}
+              </div>
             )}
           </div>
-        )}
-      </div>
 
-      {isDesktop === false && (
-        <div className="mx-auto -mb-5 mt-12 flex w-full max-w-md justify-center px-12 lg:mt-24 lg:max-w-xl">
-          <Image
-            alt="JME Film Production Company"
-            height={100}
-            src={`/images/jmills-raven-gold.svg`}
-            width={100}
-          />
-        </div>
+          {isDesktop === false && (
+            <div className="mx-auto -mb-5 mt-12 flex w-full max-w-md justify-center px-12 lg:mt-24 lg:max-w-xl">
+              <Image
+                alt="JME Film Production Company"
+                height={100}
+                src={`/images/jmills-raven-gold.svg`}
+                width={100}
+              />
+            </div>
+          )}
+          {isDesktop && (
+            <div className="mx-auto -mb-5 mt-12 w-full max-w-md px-12 lg:mt-24 lg:max-w-xl">
+              <Image
+                alt="JME Film Production Company"
+                height={202}
+                src={`/images/JME-film-prod-co-white.svg`}
+                width={600}
+              />
+            </div>
+          )}
+          <MediumWhiteBar yMargin="mb-8 mt-12 lg:mt-24" />
+        </>
       )}
-      {isDesktop && (
-        <div className="mx-auto -mb-5 mt-12 w-full max-w-md px-12 lg:mt-24 lg:max-w-xl">
-          <Image
-            alt="JME Film Production Company"
-            height={202}
-            src={`/images/JME-film-prod-co-white.svg`}
-            width={600}
-          />
-        </div>
-      )}
-      <MediumWhiteBar yMargin="mb-8 mt-12 lg:mt-24" />
     </Layout>
   )
 }
@@ -267,6 +298,13 @@ export async function getStaticProps({ params }) {
       }
     `
     )
+    const portfolioPagePassword = await getClient().fetch(
+      groq`
+      *[_type == "portfolioPage"][0]{
+        password,
+        passwordInputPrompt,
+      }`
+    )
 
     if (!portfolioItem) {
       return {
@@ -275,7 +313,7 @@ export async function getStaticProps({ params }) {
     }
 
     return {
-      props: { portfolioItem, portfolioItems },
+      props: { portfolioItem, portfolioItems, portfolioPagePassword },
     }
   } catch (error) {
     return {
--- web/pages/portfolio/index.js
+++ web/pages/portfolio/index.js
@@ -5,25 +5,19 @@ import { PortableText } from '@portabletext/react'
 import PortfolioItemTile from '@/components/portfolio-item-tile'
 import MediumWhiteBar from '@/components/medium-white-bar'
 import EmailSignupForm from '@/components/email-signup-form'
-import { useEffect, useState } from 'react'
 import classNames from 'classnames'
+import useIsLoggedIn from 'hooks/useIsLoggedIn'
 
 function Portfolio({ portfolioPage, portfolioItems }) {
-  const [loggedIn, setLoggedIn] = useState(false)
   const token = portfolioPage.password
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setLoggedIn(localStorage.getItem('private-portfolio') === token)
-    }
-  }, [token])
+  const [isLoggedIn, setIsLoggedIn] = useIsLoggedIn(token)
 
   function handleSubmit(e) {
     e.preventDefault()
 
     if (e.target.password.value === token) {
       localStorage.setItem('private-portfolio', e.target.password.value)
-      setLoggedIn(true)
+      setIsLoggedIn(token)
     }
   }
 
@@ -32,61 +26,65 @@ function Portfolio({ portfolioPage, portfolioItems }) {
       title={portfolioPage.seoTitle}
       description={portfolioPage.seoDescription}
     >
-      {!loggedIn && (
-        <div className="mx-auto max-w-3xl border py-12 text-center">
-          <p className="mb-6 text-lg font-semibold">
-            {portfolioPage.passwordInputPrompt}
-          </p>
-          <form
-            className="mx-auto flex w-1/2 items-center justify-center rounded"
-            onSubmit={handleSubmit}
-          >
-            <label className="sr-only" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="-mr-1 flex-1 rounded-l-md border-2 border-r-0 border-gray-300 bg-black bg-opacity-70 py-2 px-4 text-white focus:bg-opacity-90"
-              name="password"
-              type="password"
-              placeholder="PASSWORD"
-            />
-            <button
-              className={classNames(
-                'group flex items-center gap-x-2 rounded-md border-2 border-gray-300 px-4 py-2 transition-colors',
-                'hover:bg-gold hover:text-black'
-              )}
-              type="submit"
+      {!isLoggedIn && (
+        <div className="mx-auto min-h-screen max-w-3xl text-center">
+          <div className="border py-12">
+            <p className="mb-6 text-lg font-semibold">
+              {portfolioPage.passwordInputPrompt}
+            </p>
+            <form
+              className="mx-auto flex w-1/2 items-center justify-center rounded"
+              onSubmit={handleSubmit}
             >
-              Submit
-            </button>
-          </form>
+              <label className="sr-only" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="-mr-1 flex-1 rounded-l-md border-2 border-r-0 border-gray-300 bg-black bg-opacity-70 py-2 px-4 text-white focus:bg-opacity-90"
+                name="password"
+                type="text"
+                placeholder="PASSWORD"
+              />
+              <button
+                className={classNames(
+                  'group flex items-center gap-x-2 rounded-md border-2 border-gray-300 px-4 py-2 transition-colors',
+                  'hover:bg-gold hover:text-black'
+                )}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       )}
 
-      {loggedIn && (
-        <div className="mx-1 grid grid-cols-1 gap-1 lg:grid-cols-3">
-          {portfolioItems.map((portfolioItem, index) => {
-            return (
-              <PortfolioItemTile portfolioItem={portfolioItem} key={index} />
-            )
-          })}
-        </div>
-      )}
-      <div className="container mx-auto mt-12 px-12 text-center text-white">
-        {portfolioPage.portfolioPageDescription && (
-          <div className="prose-lg mx-auto max-w-lg border py-1 text-center">
-            <PortableText value={portfolioPage.portfolioPageDescription} />
+      {isLoggedIn && (
+        <>
+          <div className="mx-1 grid grid-cols-1 gap-1 lg:grid-cols-3">
+            {portfolioItems.map((portfolioItem, index) => {
+              return (
+                <PortfolioItemTile portfolioItem={portfolioItem} key={index} />
+              )
+            })}
           </div>
-        )}
-        <div className="mt-10">
-          <EmailSignupForm
-            title={portfolioPage.subscribeFormTitle}
-            successMessage={portfolioPage.subscribeFormSuccessMessage}
-            customReel={true}
-          />
-        </div>
-        <MediumWhiteBar yMargin="mb-8 mt-12 lg:mt-24" />
-      </div>
+          <div className="container mx-auto mt-12 px-12 text-center text-white">
+            {portfolioPage.portfolioPageDescription && (
+              <div className="prose-lg mx-auto max-w-lg border py-1 text-center">
+                <PortableText value={portfolioPage.portfolioPageDescription} />
+              </div>
+            )}
+            <div className="mt-10">
+              <EmailSignupForm
+                title={portfolioPage.subscribeFormTitle}
+                successMessage={portfolioPage.subscribeFormSuccessMessage}
+                customReel={true}
+              />
+            </div>
+            <MediumWhiteBar yMargin="mb-8 mt-12 lg:mt-24" />
+          </div>
+        </>
+      )}
     </Layout>
   )
 }

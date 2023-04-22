337b395c255e23d5c2325e74fc6fef54d9d41f67
Colton Bonner <cbonner26118@gmail.com>
2023-04-21 18:29:55 -0600

    Reverted 'background-text-section-half' to previous version, and moved new version to desktop only within services-interactive-card. Added same hover scale increase to 'servicesShortNames' buttons.

--- web/components/background-text-section-half.js
+++ web/components/background-text-section-half.js
@@ -3,32 +3,24 @@ import { PortableText } from '@portabletext/react'
 import classNames from 'classnames'
 import useIsDesktop from 'hooks/useIsDesktop'
 import Image from 'next/image'
+import { Link as SmoothScrollLink } from 'react-scroll'
 import { H3 } from './headings'
 import LittleGoldBar from './little-gold-bar'
-import ServicesThumbnails from './services-thumbnails'
 
 const BackgroundTextSectionHalf = ({
-  services,
+  image,
+  imageAlt,
+  title,
+  shortName,
+  description,
+  step,
   serviceShortNames,
-  activeService,
-  setActiveService,
 }) => {
   const isDesktop = useIsDesktop()
 
-  const service = services.at(activeService)
-
-  const { image, name: title, shortName, description } = service
-
-  const step = activeService + 1
-
-  const stepNumber = '0' + step
-
   return (
     <div
-      className={classNames(
-        'relative border border-gray-300 px-4 pt-4 pb-2',
-        'lg:px-6 lg:pt-6 lg:pb-3'
-      )}
+      className="relative border border-gray-300 p-4 lg:p-6"
       id={`service-${shortName}`}
     >
       <div className="group w-full" style={{ lineHeight: 0 }}>
@@ -36,71 +28,63 @@ const BackgroundTextSectionHalf = ({
           <div className="absolute inset-0 z-10 bg-opacity-40 bg-gradient-to-l from-black via-transparent to-transparent" />
           <div
             className={classNames(
-              `absolute top-0 right-4 bottom-0 left-auto z-10 flex flex-col justify-between gap-y-2 text-right text-xs`,
-              `lg:top-6 lg:text-sm`
+              `absolute top-0 right-4 bottom-0 left-auto z-10 flex flex-col justify-center gap-y-2 text-right text-xs`,
+              `lg:top-6 lg:justify-start lg:text-sm`
             )}
           >
-            <div>
-              <div className="hidden lg:block">
-                <h3 className="text-3xl font-bold">{title}</h3>
-                <div className="flex justify-end py-1">
-                  <LittleGoldBar />
-                </div>
-              </div>
-              <div className={classNames('grid gap-2 pt-4', 'lg:pt-2')}>
-                {serviceShortNames?.map((serviceShortName, index) => {
-                  const isChecked = index + 1 < step
-                  const isCurrent = index + 1 === step
-                  return (
-                    <button
-                      className={classNames(
-                        'flex cursor-pointer items-center justify-end gap-x-2',
-                        'lg:gap-x-4',
-                        'hover:scale-110',
-                        isCurrent ? 'font-bold text-white' : 'text-gray-200'
-                      )}
-                      onClick={() => setActiveService(index)}
-                      key={index}
-                      aria-label={`View ${title.toLowerCase()} services`}
-                    >
-                      <p
-                        className={classNames(
-                          'uppercase tracking-wider',
-                          !isCurrent && 'opacity-60',
-                          isCurrent && 'scale-110 tracking-widest'
-                        )}
-                      >
-                        {serviceShortName}
-                      </p>
-                      <svg
-                        className="h-4 w-4 lg:h-6 lg:w-6"
-                        enableBackground="new 0 0 120 120"
-                        viewBox="0 0 120 120"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        {isChecked && (
-                          <path
-                            className="fill-current text-gold"
-                            d="m5 5h110v110h-110z"
-                          />
-                        )}
-                        {isCurrent && (
-                          <path
-                            className="fill-current text-gold"
-                            d="m5 5h110v110h-110z"
-                          />
-                        )}
-                        {/* outline */}
-                        <path
-                          className="fill-current"
-                          d="m110 10v100h-100v-100zm10-10h-10-100-10v10 100 10h10 100 10v-10-100z"
-                        />
-                      </svg>
-                    </button>
-                  )
-                })}
+            <div className="hidden lg:block">
+              <h3 className="text-3xl font-bold">{title}</h3>
+              <div className="flex justify-end py-1">
+                <LittleGoldBar />
               </div>
             </div>
+            {serviceShortNames?.map((serviceShortName, index) => {
+              const isChecked = index + 1 <= step
+              const isCurrent = index + 1 === step
+              return (
+                <SmoothScrollLink
+                  to={`service-${serviceShortName}`}
+                  smooth={true}
+                  offset={-20}
+                  duration={500}
+                  className={classNames(
+                    'flex cursor-pointer items-center justify-end gap-x-2',
+                    'lg:gap-x-4',
+                    'hover:scale-110',
+                    isCurrent ? 'font-bold text-white' : 'text-gray-200'
+                  )}
+                  key={index}
+                >
+                  <p
+                    className={classNames(
+                      'uppercase tracking-wider',
+                      !isCurrent && 'opacity-60',
+                      isCurrent && 'scale-110 tracking-widest'
+                    )}
+                  >
+                    {serviceShortName}
+                  </p>
+                  <svg
+                    className="h-4 w-4 lg:h-6 lg:w-6"
+                    enableBackground="new 0 0 120 120"
+                    viewBox="0 0 120 120"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    {isChecked && (
+                      <path
+                        className="fill-current text-gold"
+                        d="m5 5h110v110h-110z"
+                      />
+                    )}
+                    {/* outline */}
+                    <path
+                      className="fill-current"
+                      d="m110 10v100h-100v-100zm10-10h-10-100-10v10 100 10h10 100 10v-10-100z"
+                    />
+                  </svg>
+                </SmoothScrollLink>
+              )
+            })}
           </div>
           <div className="absolute left-0 -top-4 z-10 pl-4 text-center lg:-top-6">
             <div className="flex flex-col items-center justify-center">
@@ -112,65 +96,19 @@ const BackgroundTextSectionHalf = ({
                   'lg:pt-8 lg:text-5xl'
                 )}
               >
-                {stepNumber}
+                0{step}
               </p>
             </div>
           </div>
-          <div className="absolute bottom-0 z-10 flex w-full justify-between px-8 pb-1 text-3xl lg:px-2 lg:pb-6">
-            <button
-              aria-label="previous service step"
-              onClick={() => {
-                if (step > 1) {
-                  setActiveService(activeService - 1)
-                } else {
-                  setActiveService(services.length - 1)
-                }
-              }}
-            >
-              <Image
-                className="opacity-50 hover:opacity-100"
-                alt="previous service step"
-                src="/images/left_arrow.svg"
-                width={isDesktop ? 37.8 : 18.9}
-                height={isDesktop ? 85.4 : 42.7}
-              />
-            </button>
-            <button
-              aria-label="next service step"
-              onClick={() => {
-                if (step < services.length) {
-                  setActiveService(activeService + 1)
-                } else {
-                  setActiveService(0)
-                }
-              }}
-            >
-              <Image
-                className="opacity-50 hover:opacity-100"
-                alt="previous service step"
-                src="/images/right_arrow.svg"
-                width={isDesktop ? 37.8 : 18.9}
-                height={isDesktop ? 85.4 : 42.7}
-              />
-            </button>
-          </div>
-          <div className="w-full">
-            <Image
-              alt={title}
-              src={`${urlForSanitySource(image)}?w=1200&h=${
-                isDesktop ? 500 : 800
-              }&auto=format&fit=crop&crop=focalpoint`}
-              height={isDesktop ? 500 : 800}
-              width={1200}
-            />
-          </div>
+          <Image
+            alt={imageAlt}
+            height={isDesktop ? 700 : 800}
+            src={`${urlForSanitySource(image)}?w=1200&h=${
+              isDesktop ? 700 : 800
+            }&auto=format&fit=crop&crop=focalpoint`}
+            width={1200}
+          />
         </div>
-        <div className="w-full border-b border-gold" />
-        <ServicesThumbnails
-          services={services || []}
-          activeService={activeService}
-          setActiveService={setActiveService}
-        />
         <div className="mx-auto flex flex-col justify-center gap-y-2">
           <H3 className="!mt-3 !mb-0 inline w-full py-1 text-center uppercase lg:hidden">
             {title}
@@ -180,7 +118,7 @@ const BackgroundTextSectionHalf = ({
               <div className="mx-auto lg:hidden">
                 <LittleGoldBar />
               </div>
-              <div className="prose-lg mx-auto max-w-5xl font-light leading-normal text-white">
+              <div className="prose-lg mx-auto mt-1.5 max-w-5xl font-light leading-normal text-white lg:pt-3">
                 <PortableText value={description} />
               </div>
             </>

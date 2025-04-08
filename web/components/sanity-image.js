c6252682150530ab682f2d9a491712a6c12cd007
Michael Bonner <michaelwbonner@gmail.com>
2025-04-07 19:20:07 -0600

    update remaining deps

--- web/components/sanity-image.js
+++ web/components/sanity-image.js
@@ -1,20 +1,40 @@
-import { useNextSanityImage } from 'next-sanity-image'
+import urlForSanitySource from '@/lib/urlForSanitySource'
 import Image from 'next/image'
-import { sanityClient } from '../lib/sanity'
+import { getImageDimensions } from '@sanity/asset-utils'
 
 function SanityImage({ alt, className, image }) {
-  const imageProps = useNextSanityImage(sanityClient, image)
+  if (!image?.asset) return null
+
+  const { height, width } = getImageDimensions(image)
 
   return (
-    // eslint-disable-next-line jsx-a11y/alt-text
     <Image
-      {...imageProps}
-      alt={alt}
       className={className}
-      fill={imageProps.width ? false : true}
-      sizes="(max-width: 800px) 100vw, 800px"
-      unoptimized
+      src={urlForSanitySource(image).width(1920).height(1080).dpr(2).url()}
+      alt={alt ?? image?.asset._ref}
+      width={width}
+      height={height}
+      placeholder="blur"
+      blurDataURL={urlForSanitySource(image)
+        .width(24)
+        .height(24)
+        .blur(10)
+        .url()}
+      sizes="
+          (max-width: 768px) 100vw,
+          (max-width: 1200px) 50vw,
+          40vw"
     />
+
+    // eslint-disable-next-line jsx-a11y/alt-text
+    // <Image
+    //   {...imageProps}
+    //   alt={alt}
+    //   className={className}
+    //   fill={imageProps.width ? false : true}
+    //   sizes="(max-width: 800px) 100vw, 800px"
+    //   unoptimized
+    // />
   )
 }
 

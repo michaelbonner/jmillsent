72a38758b7280173b526768c0115688ab2594da5
Michael Bonner <michaelwbonner@gmail.com>
2025-04-07 19:56:51 -0600

    Simplify sanity image

--- web/components/sanity-image.js
+++ web/components/sanity-image.js
@@ -1,26 +1,19 @@
 import urlForSanitySource from '@/lib/urlForSanitySource'
-import Image from 'next/image'
 import { getImageDimensions } from '@sanity/asset-utils'
+import Image from 'next/image'
 
 function SanityImage({ alt, className, image }) {
   if (!image?.asset) return null
 
   const { height, width } = getImageDimensions(image)
 
-  const trimmedWidth = width > 2000 ? 2000 : width
-  const trimmedHeight = width > 2000 ? height * (2000 / width) : height
-
   return (
     <Image
       className={className}
-      src={urlForSanitySource(image)
-        .dpr(2)
-        .width(trimmedWidth)
-        .height(trimmedHeight)
-        .url()}
+      src={urlForSanitySource(image).width(width).height(height).url()}
       alt={alt ?? image?.asset._ref}
-      width={trimmedWidth}
-      height={trimmedHeight}
+      width={width}
+      height={height}
       placeholder="blur"
       blurDataURL={urlForSanitySource(image)
         .width(24)
@@ -28,7 +21,6 @@ function SanityImage({ alt, className, image }) {
         .blur(10)
         .url()}
       sizes="(max-width: 800px) 100vw, 800px"
-      fill={width ? false : true}
     />
   )
 }

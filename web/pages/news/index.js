eb1fca5965779ba50e0bed64dd924a7a75299ec6
Michael Bonner <mike@bootpackdigital.com>
2025-07-10 14:38:31 -0600

    Fallback if news page gets unpublished

--- web/pages/news/index.js
+++ web/pages/news/index.js
@@ -8,9 +8,9 @@ function News({ newsPage }) {
   const heroContent = (
     <div className="-my-2 flex w-full items-center justify-center py-24 text-white sm:my-10 sm:mb-10">
       <div className="flex w-screen flex-col items-center justify-center px-4 text-center">
-        <H1>{newsPage.seoTitle}</H1>
+        <H1>{newsPage?.seoTitle || 'News'}</H1>
         <H2 className="font-outline uppercase sm:text-2xl lg:text-5xl">
-          {newsPage.seoDescription}
+          {newsPage?.seoDescription || 'News'}
         </H2>
       </div>
     </div>
@@ -18,15 +18,15 @@ function News({ newsPage }) {
 
   return (
     <Layout
-      title={newsPage.seoTitle}
-      description={newsPage.seoDescription}
-      heroImageUrl={newsPage.poster}
+      title={newsPage?.seoTitle || 'News'}
+      description={newsPage?.seoDescription || 'News'}
+      heroImageUrl={newsPage?.poster || ''}
       heroContent={heroContent}
     >
       <div className="mx-auto max-w-7xl lg:mt-10">
         <div className="mx-4 mt-8 rounded-2xl bg-white lg:mx-8">
           <div className="grid max-w-7xl justify-center gap-y-24 rounded-2xl p-6 lg:gap-y-10 lg:bg-white lg:p-10">
-            {newsPage?.newsItems.map((newsItem, index) => {
+            {(newsPage?.newsItems ?? []).map((newsItem, index) => {
               return <NewsItemCard key={index} newsItem={newsItem} />
             })}
           </div>

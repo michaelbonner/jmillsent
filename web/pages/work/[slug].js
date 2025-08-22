cdd80a2314e864bb190d470a888393abb21007db
Michael Bonner <mike@bootpackdigital.com>
2025-08-21 21:40:28 -0600

    Fix title generation

--- web/pages/work/[slug].js
+++ web/pages/work/[slug].js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { ClientOnly } from '@/components/client-only'
 import { H3 } from '@/components/headings'
 import Layout from '@/components/layout'
@@ -11,35 +10,25 @@ import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { useState } from 'react'
 
-const VideoPlayer = dynamic(() => import('@/components/video-player'), {})
+const VideoPlayer = dynamic(() => import('@/components/video-player'))
 
-/*
-prevent purging of aspect ratio
-aspect-w-1	aspect-h-1
-aspect-w-2	aspect-h-2
-aspect-w-3	aspect-h-3
-aspect-w-4	aspect-h-4
-aspect-w-5	aspect-h-5
-aspect-w-6	aspect-h-6
-aspect-w-7	aspect-h-7
-aspect-w-8	aspect-h-8
-aspect-w-9	aspect-h-9
-aspect-w-10	aspect-h-10
-aspect-w-11	aspect-h-11
-aspect-w-12	aspect-h-12
-aspect-w-13	aspect-h-13
-aspect-w-14	aspect-h-14
-aspect-w-15	aspect-h-15
-aspect-w-16	aspect-h-16
-*/
+const getFullTitle = ({ clientName, title }) => {
+  if (clientName && title) {
+    return `${clientName} | ${title}`
+  }
+
+  if (clientName) {
+    return clientName
+  }
+
+  return title ?? 'Work'
+}
 
 const WorkItem = ({ workItem = {} }) => {
   const [isOpen, setIsOpen] = useState(false)
   const isDesktop = useIsDesktop()
 
-  const fullTitle = workItem.clientName
-    ? `${workItem.clientName} | ${workItem.title}`
-    : workItem.title
+  const fullTitle = getFullTitle(workItem)
 
   const column1Credits = workItem?.credits?.length
     ? workItem.credits.slice(0, Math.ceil(workItem.credits.length / 2))
@@ -351,3 +340,23 @@ export async function getStaticProps({ params }) {
 }
 
 export default WorkItem
+
+/*
+prevent purging of aspect ratio
+aspect-w-1	aspect-h-1
+aspect-w-2	aspect-h-2
+aspect-w-3	aspect-h-3
+aspect-w-4	aspect-h-4
+aspect-w-5	aspect-h-5
+aspect-w-6	aspect-h-6
+aspect-w-7	aspect-h-7
+aspect-w-8	aspect-h-8
+aspect-w-9	aspect-h-9
+aspect-w-10	aspect-h-10
+aspect-w-11	aspect-h-11
+aspect-w-12	aspect-h-12
+aspect-w-13	aspect-h-13
+aspect-w-14	aspect-h-14
+aspect-w-15	aspect-h-15
+aspect-w-16	aspect-h-16
+*/
